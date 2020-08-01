a9278b7fee06e3adde719aac60f40c809ca335c0
Brilliant Djaka Irfanuddin Rofiq <brian.rofiq@gmail.com>
2020-08-01 16:28:57 +0700

    add port

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ server.post('/todo',insertTodo);
 server.del('/todo',deleteTodo);
 server.get('/todo/:_id',findData)
 
-server.listen(8080, function () {
+server.listen(process.env.PORT ||  8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
