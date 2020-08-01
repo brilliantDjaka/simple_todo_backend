c7695e542006263a8011e6e3315ae241161c2aaf
Brilliant Djaka Irfanuddin Rofiq <brian.rofiq@gmail.com>
2020-08-01 22:57:34 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 var restify = require('restify');
+var corsMiddleware = require('restify-cors-middleware');
+const cors = corsMiddleware({  
+    origins: ["*"],
+    allowHeaders: ["Authorization"],
+    exposeHeaders: ["Authorization"]
+});
 const { insertTodo, deleteTodo, checkTodo } = require('./modules/Todo/TodoCommand')
 const { findData } = require('./modules/Todo/TodoQuery')
 function respond(_req, res, next) {
@@ -9,6 +15,8 @@ function respond(_req, res, next) {
 var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
+server.pre(cors.preflight);  
+server.use(cors.actual);  
 server.get('/', respond);
 server.post('/todo',insertTodo);
 server.del('/todo/:_id',deleteTodo);
