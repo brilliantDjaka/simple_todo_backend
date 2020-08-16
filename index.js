abb7b7be09236db438732b1ee7f7c3ee8162c447
Brilliant Djaka Irfanuddin Rofiq <brian.rofiq@gmail.com>
2020-08-16 20:27:59 +0700

    implement view engine

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 var restify = require('restify');
 var corsMiddleware = require('restify-cors-middleware');
-const cors = corsMiddleware({  
+const render = require('restify-render-middleware')
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({ node: '' })// TODO fill with url
+const cors = corsMiddleware({
     origins: ["*"],
     allowHeaders: ["Authorization"],
     exposeHeaders: ["Authorization"]
@@ -15,14 +18,44 @@ function respond(_req, res, next) {
 var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
-server.pre(cors.preflight);  
-server.use(cors.actual);  
+server.pre(cors.preflight);
+server.use(cors.actual);
+server.use(render({
+    engine: 'ejs',
+    dir: __dirname + '/views'
+}))
 server.get('/', respond);
-server.post('/todo',insertTodo);
-server.del('/todo/check/:author',deleteTodo);
-server.get('/todo/:author',findData)
-server.post('/todo/check/:_id',checkTodo);
+// API
+server.post('/todo', insertTodo);
+server.del('/todo/check/:author', deleteTodo);
+server.get('/todo/:author', findData)
+server.post('/todo/check/:_id', checkTodo);
 
-server.listen(process.env.PORT ||  8080, function () {
+// Front End
+server.get('/view/:author', async (req, res, next) => {
+let data = [];
+    await findData({
+        params: {
+            author: req.params.author
+        }
+    },{
+        send:(result) => {
+            if(result.data){
+                data = result.data
+            }
+        }
+    })
+    res.render('index.ejs', {
+        author: req.params.author,
+        data:data,
+    });
+})
+server.post('/search',(req,res,next) =>{
+    const searchText = req.body["search-text"];
+
+    res.render('search.ejs',{searchText})
+})
+
+server.listen(process.env.PORT || 8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
